6f6131836dab90a1c2bb556cd06e71a8c14fbfed
Jesus Rosales <jr.jobs.1509@gmail.com>
2026-05-19 14:55:05 +0100

    fixing

---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jericard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/28 14:20:26 by jericard          #+#    #+#             */
+/*   Updated: 2025/08/28 14:20:30 by jericard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+/*
+int	main(void)
+{
+	int n = 0;
+
+	int *puntero = &n;
+	int **puntero_2 = &puntero;
+	int ***puntero_3 = &puntero_2;
+	int ****puntero_4 = &puntero_3;
+	int *****puntero_5 = &puntero_4;
+	int ******puntero_6 = &puntero_5;
+	int *******puntero_7 = &puntero_6;
+	int ********puntero_8 = &puntero_7;
+	int *********puntero_9 = &puntero_8;
+
+	printf("%d\n",n);
+	ft_ultimate_ft(puntero_9);
+	printf("%d",n);
+}
+*/
